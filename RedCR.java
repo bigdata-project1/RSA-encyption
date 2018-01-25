832cacd619ba4834a58792e14456ca9ed6c0b2cc
daneena-22 <35718418+daneena-22@users.noreply.github.com>
2018-01-25 09:28:33 +0530

    Add files via upload

---
+++ RedCR.java
@@ -0,0 +1,24 @@
+
+import java.io.IOException; 
+import org.apache.hadoop.io.*;  		  
+import org.apache.hadoop.mapreduce.*; 
+
+public class RedCR extends Reducer<Text,IntWritable,Text,IntWritable> {
+	private IntWritable result = new IntWritable();
+
+	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+		int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			/*while(values.iterator().hasNext()){
+				IntWritable i=values.iterator().next();
+				sum+=i.get();
+			}*/
+			result.set(sum);
+			context.write(key, result);
+			 
+			
+			
+	}
+}
